83a05412f49a40e1a44ccad19b5f3862fe490d8a
Jesse Qin <itzjesse@gmail.com>
2016-02-17 02:25:29 -0800

    test

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res) {
-    var rand = Math.random();
-    console.log(rand);
-    if (rand > 0.5) {
+    // var rand = Math.random();
+    // console.log(rand);
+    // if (rand > 0.5) {
+    //     projects.grid = false;
+    //     res.render('index', projects);
+    // } else {
+    //     res.redirect('/grid');
+    // }
         projects.grid = false;
         res.render('index', projects);
-    } else {
-        res.redirect('/grid');
-    }
-
 
 };
 
